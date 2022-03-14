e4efb0a50f9f2f082b92c0bf001e5691f67f1433
haeramkeem <ewqdsacxz2345@gmail.com>
2022-03-14 16:19:14 +0900

    fix: metallb-prac.sh: cleanup

--- ch3/3.3.4-LoadBalancer/metallb-prac.sh
+++ ch3/3.3.4-LoadBalancer/metallb-prac.sh
@@ -6,6 +6,7 @@
 #################################################################################
 
 # Create deployment for testing
+echo "----- DEPLOYMENT FOR TESTING -----"
 kubectl create deployment lb-hname-pods --image=sysnet4admin/echo-hname
 kubectl create deployment lb-ip-pods --image=sysnet4admin/echo-ip
 
@@ -13,42 +14,62 @@ kubectl create deployment lb-ip-pods --image=sysnet4admin/echo-ip
 kubectl scale deployment lb-hname-pods --replicas=2
 
 # Show creation result
-echo "\n----- DEPLOYMENT FOR TESTING -----"
 kubectl get pods
+echo ""
 
 # Create MetalLB pods (contoller and speakers)
 #   1 controller and 2 speakers will be created
-kubectl apply -f https://raw.githubusercontent.com/haeramkeem/k8s-exercise/main/ch3/3.3.4-LoadBalancer/metallb.yaml
+echo "----- METALLB PODS -----"
+METALLB_URL=https://raw.githubusercontent.com/haeramkeem/k8s-exercise/main/ch3/3.3.4-LoadBalancer/metallb.yaml
+kubectl apply -f $METALLB_URL
 
 # Show creation result
-echo "\n----- METALLB PODS -----"
 kubectl get pods -n metallb-system -o wide
+echo ""
 
 # Create ConfigMap for MetalLB
-kubectl apply -f https://raw.githubusercontent.com/haeramkeem/k8s-exercise/main/ch3/3.3.4-LoadBalancer/metallb-l2config.yaml
+echo "----- METALLB CONFIGMAP -----"
+CONFIG_URL=https://raw.githubusercontent.com/haeramkeem/k8s-exercise/main/ch3/3.3.4-LoadBalancer/metallb-l2config.yaml
+kubectl apply -f $CONFIG_URL
 
 # Show creation result
-echo "\n----- METALLB CONFIGMAP -----"
 kubectl get configmap -n metallb-system
+echo ""
 
 # Expose each deployment to LoadBalancer
+echo "----- EXPOSE TESTING DEPLOYMENT -----"
 kubectl expose deployment lb-hname-pods --type=LoadBalancer --name=lb-hname-svc --port=80
 kubectl expose deployment lb-ip-pods --type=LoadBalancer --name=lb-ip-svc --port=80
+
+# Show exposure result
+kubectl get services
+echo ""
 
 # Wait 1 minute before testing to finish setup
 echo "Wait 1 minute before testing..."
 sleep 1m
+echo ""
 
 # Test Load Balancer
 HNAME_EIP=$(kubectl get service lb-hname-svc -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
 IP_EIP=$(kubectl get service lb-ip-svc -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
-echo "\n----- BALANCING DEPLOYMENT TEST -----"
-echo "Request to 'lb-hname-svc': $(curl $HNAME_EIP)"
-echo "Request to 'lb-ip-svc': $(curl $IP_EIP)"
+echo "----- BALANCING DEPLOYMENT TEST -----"
+echo -s "Request to 'lb-hname-svc': $(curl $HNAME_EIP)"
+echo -s "Request to 'lb-ip-svc': $(curl $IP_EIP)"
+echo ""
 
-echo "\n----- BALANCING POD TEST -----"
+echo "----- BALANCING POD TEST -----"
 for (( i=0; i<=10; i++ ))
 do
-    curl $HNAME_EIP
+    curl -s $HNAME_EIP
 done
+echo ""
+
+# Cleanup
+kubectl delete deployment lb-hname-pods
+kubectl delete deployment lb-ip-pods
+kubectl delete service lb-hname-svc
+kubectl delete service lb-ip-svc
+kubectl delete -f $METALLB_URL
+kubectl delete -f $CONFIG_URL
