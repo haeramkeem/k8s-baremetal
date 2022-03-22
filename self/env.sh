9e68a3b5e33feaa8be6c642f013896d3f68bc0d7
haeramkeem <ewqdsacxz2345@gmail.com>
2022-03-22 12:00:21 +0900

    feat: practice environment sh

---
+++ self/env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Download environment configuration
+git clone https://github.com/haeramkeem/k8s-exercise.git
+cd ./k8s-exercise/self
+
+# Initiate provision with Virtual Box and Vagrant
+vagrant up
--- self/ping_2_nds.sh
+++
@@ -1,4 +0,0 @@
-# ping 3 times per nodes
-ping 192.168.1.101 -c 3
-ping 192.168.1.102 -c 3
-ping 192.168.1.103 -c 3
