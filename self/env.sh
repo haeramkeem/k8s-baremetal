892e72efd688a9aee3463079a0ebef3fbc9c6e24
haeramkeem <ewqdsacxz2345@gmail.com>
2022-03-23 11:45:32 +0900

    style: rename files

--- self/env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Download environment configuration
-git clone https://github.com/haeramkeem/k8s-exercise.git
-cd ./k8s-exercise/self
-
-# Initiate provision with Virtual Box and Vagrant
-vagrant up
--- testing-env/online/config.sh
+++ testing-env/online/config.sh
@@ -2,10 +2,13 @@
 
 # vim configuration 
 echo 'alias vi=vim' >> /etc/profile
+rm -rf ~/.vimrc
+curl https://raw.githubusercontent.com/haeramkeem/rcs/main/.min.vimrc > ~/.vimrc
 
-# swapoff -a to disable swapping
+# Kubernetes requires the disabling of the partition swapping
+#   swapoff -a to disable swapping
 swapoff -a
-# sed to comment the swap partition in /etc/fstab
+#   sed to comment the swap partition in /etc/fstab
 sed -i.bak -r 's/(.+ swap .+)/#\1/' /etc/fstab
 
 # kubernetes repo
@@ -21,21 +24,28 @@ gpgkey=https://${gg_pkg}/yum-key.gpg https://${gg_pkg}/rpm-package-key.gpg
 EOF
 
 # Set SELinux in permissive mode (effectively disabling it)
+#   SELinux means Security Enhanced Linux which is security-enhanced version of linux kernel
+#       In RHEL/CentOS, SELinux is installed and running on 'enforce' mode by default
+#       The 'enforce' mode rejects the insecured operations
+#       And 'permissive' mode allows the insecured operations but leave the audit to the log
+#       By 'disable' mode, u can disable the SELinux
 setenforce 0
 sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 
-# RHEL/CentOS 7 have reported traffic issues being routed incorrectly due to iptables bypassed
+# Use iptables and br_netfilter kernel module for IPv4 & IPv6 routing table
+#   RHEL/CentOS 7 have reported traffic issues being routed incorrectly due to iptables bypassed
 cat <<EOF >  /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
 modprobe br_netfilter
 
-# local small dns & vagrant cannot parse and delivery shell code.
+# Set local DNS - this will make communication between nodes with hostname instead of IP
+#   vagrant cannot parse and delivery shell code.
 echo "192.168.1.10 m-k8s" >> /etc/hosts
 for (( i=1; i<=$1; i++  )); do echo "192.168.1.10$i w$i-k8s" >> /etc/hosts; done
 
-# config DNS  
+# Config DNS server for external communication
 cat <<EOF > /etc/resolv.conf
 nameserver 1.1.1.1 #cloudflare DNS
 nameserver 8.8.8.8 #Google DNS
