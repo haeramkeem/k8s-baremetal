328912375c1e492749311ba318c678effd4e61e5
haeramkeem <ewqdsacxz2345@gmail.com>
2022-03-25 10:04:34 +0900

    style: filename

--- testing-env/offline/setup_nodes.sh
+++ testing-env/offline/setup_nodes.sh
@@ -69,5 +69,5 @@ else
     # join kubernetes cluster
     kubeadm join\
         --token 123456.1234567890123456 \
-        --discovery-token-unsafe-skip-ca-verification 192.168.1.4:6443
+        --discovery-token-unsafe-skip-ca-verification 192.168.1.10:6443
 fi
