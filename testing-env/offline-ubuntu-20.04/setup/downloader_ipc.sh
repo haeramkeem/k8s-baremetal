40b7957f3bb466ff12a06fb8c3ef76c6ae11e569
haeramkeem <ewqdsacxz2345@gmail.com>
2022-03-31 17:12:23 +0900

    fix: run script only root mode

--- testing-env/offline-ubuntu-20.04/setup/downloader_ipc.sh
+++ testing-env/offline-ubuntu-20.04/setup/downloader_ipc.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# Check superuser
+if [[ $(whoami) != "root" ]]
+then
+    echo "Please run this script in superuser."
+    echo "recommend: 'sudo su'"
+    exit 1
+fi
+
 ################################
 #  INSTALL RELATED REPOSITORY  #
 ################################
@@ -9,15 +17,15 @@ apt-get update
 apt-get install -y ca-certificates curl gnupg lsb-release apt-transport-https
 
 # Install docker apt repo
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture)\
     signed-by=/usr/share/keyrings/docker-archive-keyring.gpg]\
     https://download.docker.com/linux/ubuntu\
-    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Install kubernetes apt repo
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
 
 # Update apt
 apt-get update
