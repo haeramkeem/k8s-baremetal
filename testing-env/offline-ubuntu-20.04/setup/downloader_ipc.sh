cc1f5055c41d6ad66bb487c6fbb2f713f8118394
@haeramkeem <ewqdsacxz2345@gmail.com>
2022-04-01 17:19:29 +0900

    Update downloader_ipc.sh

--- testing-env/offline-ubuntu-20.04/setup/downloader_ipc.sh
+++ testing-env/offline-ubuntu-20.04/setup/downloader_ipc.sh
@@ -54,8 +54,9 @@ mkdir -pv $IMG_PATH
 DOCKER_CE=$(grep "docker-ce:" meta.yaml | awk '{print $2}')
 DOCKER_CLI=$(grep "docker-ce-cli:" meta.yaml | awk '{print $2}')
 CONTAINERD=$(grep "containerd-io:" meta.yaml | awk '{print $2}')
-#   download the packages will all dependencies included from apt: https://stackoverflow.com/a/45489718
-#       + `grep -v "i386"` will discard all dependencies with i386 architecture
+#   download the packages will all dependencies included from apt
+#       `grep -v "i386"` will discard all dependencies with i386 architecture
+#       ref: https://stackoverflow.com/a/45489718
 DOCKER_PKGS="docker-ce=$DOCKER_CE docker-ce-cli=$DOCKER_CLI containerd.io=$CONTAINERD"
 apt-get download $(apt-cache depends --recurse --no-recommends --no-suggests \
     --no-conflicts --no-breaks --no-replaces --no-enhances \
