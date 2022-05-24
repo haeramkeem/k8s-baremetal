eee36924d3294994fb29d566636c7d12396f0486
haeramkeem <ewqdsacxz2345@gmail.com>
2022-05-24 11:20:02 +0900

    feat: offline/external-HAProxy-IC

--- offline-k8s-setup/ubuntu-20.04/external-HAProxy-IC/src/setup_ingress_controller.sh
+++ offline-k8s-setup/ubuntu-20.04/external-HAProxy-IC/src/setup_ingress_controller.sh
@@ -10,24 +10,27 @@ systemctl disable haproxy
 # Allow the haproxy binary to bind to ports 80 and 443:
 setcap cap_net_bind_service=+ep /usr/sbin/haproxy
 
-# Install the HAProxy Kubernetes Ingress Controller
-cp $ROOT/bin/haproxy-ingress-controller /usr/local/bin/
+# Install HAProxy Kubernetes Ingress Controller
+cp -v $ROOT/bin/haproxy-ingress-controller /usr/local/bin/
 
-cp $ROOT/service/haproxy-ingress.service /lib/systemd/system/
+# Install HAProxy service
+cp -v $ROOT/etc/haproxy.cfg /etc/haproxy/haproxy.cfg.bak
+cp -v $ROOT/etc/haproxy-ingress.service /lib/systemd/system/
 systemctl enable haproxy-ingress
 systemctl start haproxy-ingress
 
-# Copy kube config to this server
+# Copy kube config to this cluster
 CP_UNAME="blue"
 CP_IP="192.168.1.101"
 mkdir -p /root/.kube
-scp ${CP_UNAME}@${CP_IP}:/etc/kubernetes/admin.conf /root/.kube/config
+scp ${CP_UNAME}@${CP_IP}:/home/blue/.kube/config /root/.kube/config
 chown -R root:root /root/.kube
+systemctl restart haproxy-ingress
 
 # Install Bird
 dpkg -i $ROOT/deb/bird/*.deb
 
 # Copy over bird.conf
-cp $ROOT/service/bird.conf /etc/bird/bird.conf
+cp $ROOT/etc/bird.conf /etc/bird/bird.conf
 sudo systemctl enable bird
 sudo systemctl restart bird
