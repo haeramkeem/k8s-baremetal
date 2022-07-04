4aa06499b872cab11f7cff80d2802c8fe3c464de
haeramkeem <ewqdsacxz2345@gmail.com>
2022-07-04 17:11:10 +0900

    fix: OFFLINE/RHEL8/HA check_apiserver.sh to check exact vip

--- offline-cluster/rocky-8.6/high-available/src/check_apiserver.sh
+++ offline-cluster/rocky-8.6/high-available/src/check_apiserver.sh
@@ -6,6 +6,6 @@ errorExit() {
 }
 
 curl --silent --max-time 2 --insecure https://localhost:${APISERVER_DEST_PORT}/ -o /dev/null || errorExit "Error GET https://localhost:${APISERVER_DEST_PORT}/"
-if ip addr | grep -q ${APISERVER_VIP}; then
+if ip addr | grep -q ${APISERVER_VIP}/; then
     curl --silent --max-time 2 --insecure https://${APISERVER_VIP}:${APISERVER_DEST_PORT}/ -o /dev/null || errorExit "Error GET https://${APISERVER_VIP}:${APISERVER_DEST_PORT}/"
 fi
